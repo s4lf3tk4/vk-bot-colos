fa5fc7f3ff3cc7e50449cfb29a09d9d29b8616e1
s4lf3tk4 <shonriq28@gmail.com>
2026-07-04 21:48:42 +0300

    Delete config/Config.php

--- config/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors', 0);
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/../logs/php_errors.log');
-
-define('VK_CONFIRMATION_CODE', 'e29a3fd2');
-define('VK_TOKEN', 'vk1.a.AmQv7f8WIl0Jvb4OyypgU7nSn53N8yVLDrcdBPbvdtYak6E2mLgM17IWqeXR6zAtC0xMHwGRiMcpezibuQYMt-r2NrUu2kq5rPWa1sjwyXZSLhqcb1nTscWX3MCK4JV1g4oVkBHlYOYzfFplKobu5kH9zrOulOu1WbczdKjxOylNXxdlA7Ag7rElqO06rybl0WTXwQVTdGZza5s58aRIZg');
-
-?>
